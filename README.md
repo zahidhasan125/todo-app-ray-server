6bada9c701aa09cb05e07b75c09604bef9fa51fa
zahidhasan125 <zahidhasan125@gmail.com>
2023-04-27 21:12:49 +0600

    mongodb connected, simple post request created for create a todo

---
+++ README.md
@@ -0,0 +1,8 @@
+# Welcome to my ToDo App
+
+Clone the repository and run `npm install` in terminal to install all the dependencies.
+
+You can start the app by running the command `npm run start`
+
+Create a .env file and add this lines with your username and password:
+- MONGO_URI = mongodb+srv://<username>:<password>@cluster0.voxvdqi.mongodb.net/?retryWrites=true&w=majority
--- index.js
+++ index.js
@@ -3,11 +3,50 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.voxvdqi.mongodb.net/?retryWrites=true&w=majority`;
+
 app.use(cors());
 app.use(express.json());
-require('dotenv').config();
 
 
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
+async function run() {
+    try {
+
+        const todoCollections = client.db('RayToDo').collection('todoCollection');
+
+        // CRUD for todo app
+
+        // Create todo
+
+        app.post('/create', async (req, res) => {
+            const newToDo = req.body;
+            const result = await todoCollections.insertOne(newToDo);
+            res.send(result);
+        })
+
+
+
+    } finally {
+
+    }
+}
+run()
+    .catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Server Running');
+});
 app.listen(PORT, () => console.log(`Listening on: ${PORT}`));
