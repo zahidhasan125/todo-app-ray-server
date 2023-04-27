5668339be520a0a507f80192e46993ddb3691fa7
zahidhasan125 <zahidhasan125@gmail.com>
2023-04-28 00:54:40 +0600

    jsonwebtoken install and implemented, created jwt endpoint

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 5000;
-
 require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.voxvdqi.mongodb.net/?retryWrites=true&w=majority`;
+
 
 app.use(cors());
 app.use(express.json());
 
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.voxvdqi.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -21,10 +22,46 @@ const client = new MongoClient(uri, {
     }
 });
 
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized!' });
+    }
+
+    const jwtToken = authHeader.split(' ')[1];
+
+    jwt.verify(jwtToken, process.env.TOKEN_SECRET, (tokenError, decoded) => {
+        if (tokenError) {
+            return res.status(403).send({ message: "You're not authorized to apply the changes." });
+        }
+        req.decoded = decoded;
+        next();
+    });
+};
+
+
 async function run() {
     try {
-
+        // todo collection
         const todoCollections = client.db('RayToDo').collection('todoCollection');
+        const userCollections = client.db('RayToDo').collection('userCollection');
+
+        // jwt auth token endpoint
+        
+        app.get('/jwt', async (req, res) => {
+            const user = req.query.email;
+            const query = { user };
+            const userExist = await userCollections.findOne(query);
+
+            if (!userExist) {
+                return res.status(401).send({ message: 'Unauthorized!' });
+            }
+            
+            const token = jwt.sign({ user }, process.env.TOKEN_SECRET, { expiresIn: '7 days' });
+            res.send({ todoAccessToken: token });
+        })
+
 
         // CRUD for todo app
 
